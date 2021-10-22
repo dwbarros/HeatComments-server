c7901a5c30c681f01f90bc37a2cf7fdd351d9126
David Barros <dwbarros@gmail.com>
2021-10-22 01:56:10 +0100

    Route to return que user profile.

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticateUserService';
 
 const router = Router();
@@ -20,6 +21,12 @@ router.post(
 router.get(
     "/messages/last3", 
     new GetLast3MessagesController().handle
+);
+
+router.get(
+    "/profile",
+    ensureAuthenticated,
+    new ProfileUserController().handle
 );
 
 export { router };
