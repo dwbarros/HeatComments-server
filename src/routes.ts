1f17c64e8ab5cdb46e300a7de1a51e460da263fc
David Barros <dwbarros@gmail.com>
2021-10-22 01:35:34 +0100

    Route that retuns the last 3 messages.

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
 import { ensureAuthenticated } from './middleware/ensureAuthenticateUserService';
 
 const router = Router();
@@ -14,6 +15,11 @@ router.post(
     "/messages",
     ensureAuthenticated,
     new CreateMessageController().handle
+);
+
+router.get(
+    "/messages/last3", 
+    new GetLast3MessagesController().handle
 );
 
 export { router };
